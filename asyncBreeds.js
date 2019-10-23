c2fe0e01e2d9afd519e7adfc3a0dd441685a9265
dbeattie <darren@fettle.ca>
2019-10-23 01:04:24 +0000

    built a synchronous and asynchronus cat breed function with mocha-chai test cases and debugging of asyncBreed.js mostly

---
+++ asyncBreeds.js
@@ -0,0 +1,23 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log('Callback: I have the data!')
+    if (!error) {
+      callback(data);
+    } else {
+      callback(data) == undefined
+    } 
+  }) 
+};
+
+// we try to get the return value
+// breedDetailsFromFile('Bombay', (bombay) => {
+//   console.log('Return Value: ', bombay) // => print out details correctly.
+// });
+
+module.exports = breedDetailsFromFile;
+// const bombay = breedDetailsFromFile('Bombay');
+// console.log('Return Value: ', bombay) // => will NOT print out details, instead we will see undefined!
