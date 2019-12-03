114f629e7ceb134112e4ffba3cb18db3278fb41d
dbeattie <darren@fettle.ca>
2019-12-03 21:11:50 +0000

    cleaned up some comments

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,15 +1,3 @@
-// const fs = require('fs');
-
-// const breedDetailsFromFile = function(breed, cb) {
-//   console.log(`breedDetailsFromFile: Calling readFile...`);
-//   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-//     console.log('Callback: I have the data!')
-//     if (!error) cb(data); //assuming no error, pass the data into the callback
-//   });
-// }
-
-
-// asyncBreeds.js
 const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, callback) {
@@ -24,11 +12,9 @@ const breedDetailsFromFile = function(breed, callback) {
   });
 };
 
-// we try to get the return value
+// INITIAL GIVEN TESTING THAT WAS PORTED OVER TO TEST FILE
 // breedDetailsFromFile('Bombay', (bombay) => {
 //   console.log('Return Value: ', bombay) // => print out details correctly.
 // });
 
 module.exports = breedDetailsFromFile;
-// // const bombay = breedDetailsFromFile('Bombay');
-// // console.log('Return Value: ', bombay) // => will NOT print out details, instead we will see undefined!
