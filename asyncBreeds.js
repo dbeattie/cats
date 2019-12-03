4f3c9eadf856babb9cbf59795929765200f01d5c
dbeattie <darren@fettle.ca>
2019-12-03 21:09:14 +0000

    resolved a bug that did not return anything in an async test case for undefined
    created several files and new test cases to resolves issues given in an assignment

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,16 +1,27 @@
+// const fs = require('fs');
+
+// const breedDetailsFromFile = function(breed, cb) {
+//   console.log(`breedDetailsFromFile: Calling readFile...`);
+//   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+//     console.log('Callback: I have the data!')
+//     if (!error) cb(data); //assuming no error, pass the data into the callback
+//   });
+// }
+
+
 // asyncBreeds.js
 const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, callback) {
-  console.log('breedDetailsFromFile: Calling readFile...');
+  console.log(`breedDetailsFromFile: Calling readFile...`);
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log('Callback: I have the data!')
+    console.log('Callback: I have the data!');
     if (!error) {
       callback(data);
     } else {
-      callback(data) == undefined
-    } 
-  }) 
+      callback(undefined);
+    }
+  });
 };
 
 // we try to get the return value
@@ -19,5 +30,5 @@ const breedDetailsFromFile = function(breed, callback) {
 // });
 
 module.exports = breedDetailsFromFile;
-// const bombay = breedDetailsFromFile('Bombay');
-// console.log('Return Value: ', bombay) // => will NOT print out details, instead we will see undefined!
+// // const bombay = breedDetailsFromFile('Bombay');
+// // console.log('Return Value: ', bombay) // => will NOT print out details, instead we will see undefined!
